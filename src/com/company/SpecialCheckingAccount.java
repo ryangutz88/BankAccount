137bb8ebdcc2f9f31eed6bbb67f386ecdff76ac1
Ryan Gutierrez <ryangutierrez@billericak12.com>
2020-03-09 12:30:33 -0400

    poop4

--- src/com/company/SpecialCheckingAccount.java
+++ src/com/company/SpecialCheckingAccount.java
@@ -1,4 +1,17 @@
 package com.company;
 
-public class SpecialCheckingAccount extends CheckingAccount() {
+public class SpecialCheckingAccount extends CheckingAccount{
+    private double interestRate;
+    private double minimumInterest;
+
+    public SpecialCheckingAccount(int idNumber, double startBalance, double cc, double ir, double minBal){
+        super(idNumber, startBalance, cc);
+        interestRate = ir;
+        minimumInterest = minBal;
+    }
+
+    @Override
+    public void clearCheck(double amount) {
+        super.clearCheck(amount);
+    }
 }
